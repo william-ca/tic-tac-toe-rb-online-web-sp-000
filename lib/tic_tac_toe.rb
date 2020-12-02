3702f8a63b8e21972585c3950a04f147759901e3
Will Andre <wandre94@gmail.com>
2020-12-02 00:32:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,8 +62,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, turn_count)
-  board[index] = current_player(board)
+def move(board, index, value)
+  board[index] = turn_count(board)
 end
 
 # Define your WIN_COMBINATIONS constant
