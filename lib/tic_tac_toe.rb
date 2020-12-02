8181f6265d2e852517824ab482135ff7ca3f34e0
Will Andre <wandre94@gmail.com>
2020-12-02 00:18:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,7 +7,11 @@ def display_board(board)
 end
 
 def current_player(board)
-   turn_count(board) % 2 == 0 ? "X" : "O"
+  if turn_count(board).even?
+    "X"
+  elsif turn_count(board).odd?
+    "O"
+  end
 end
 
 def play(board)
