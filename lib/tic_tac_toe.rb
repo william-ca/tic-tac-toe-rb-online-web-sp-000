8b74ec8e9d0ce7a408dcc420673593f2e710d588
Will Andre <wandre94@gmail.com>
2020-12-02 00:29:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player = "X")
+def move(board, index, "X")
   board[index] = current_player(board)
 end
 
